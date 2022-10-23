5a89e4663d697d76ac06f8e02dcc7649db8be289
carreljo11 <61338497+carreljo11@users.noreply.github.com>
2022-10-23 15:51:50 -0400

    Add files via upload

---
+++ nodeExercises.js
@@ -0,0 +1,106 @@
+/** Part 1 */
+const fs = require('fs');
+const process = require('process');
+const axios = require('axios');
+
+function cat(path) {
+  fs.readFile(path, 'utf8', function(err, data) {
+    if (err) {
+      console.error(`Error reading ${path}: ${err}`);
+      process.exit(1);
+    } else {
+      console.log(data);
+    }
+  });
+}
+
+cat(process.argv[2]);
+
+/** Part 2 */
+function cat(path) {
+    fs.readFile(path, 'utf8', function(err, data) {
+      if (err) {
+        console.error(`Error reading ${path}: ${err}`);
+        process.exit(1);
+      } else {
+        console.log(data);
+      }
+    });
+  }
+  
+  /** read page at URL and print it out. */
+  
+  async function webCat(url) {
+    try {
+      let resp = await axios.get(url);
+      console.log(resp.data);
+    } catch (err) {
+      console.error(`Error fetching ${url}: ${err}`);
+      process.exit(1);
+    }
+  }
+  
+  let path = process.argv[2];
+  
+  if (path.slice(0, 4) === 'http') {
+    webCat(path);
+  } else {
+    cat(path);
+  }
+
+  /** Part 3 */
+
+  function handleOutput(text, out) {
+    if (out) {
+      fs.writeFile(out, text, 'utf8', function(err) {
+        if (err) {
+          console.error(`Couldn't write ${out}: ${err}`);
+          process.exit(1);
+        }
+      });
+    } else {
+      console.log(text);
+    }
+  }
+  
+  /** read file at path and print it out. */
+  
+  function cat(path, out) {
+    fs.readFile(path, 'utf8', function(err, data) {
+      if (err) {
+        console.error(`Error reading ${path}: ${err}`);
+        process.exit(1);
+      } else {
+        handleOutput(data, out);
+      }
+    });
+  }
+  
+  /** read page at URL and print it out. */
+  
+  async function webCat(url, out) {
+    try {
+      let resp = await axios.get(url);
+      handleOutput(resp.data, out);
+    } catch (err) {
+      console.error(`Error fetching ${url}: ${err}`);
+      process.exit(1);
+    }
+  }
+  
+  let path;
+  let out;
+  
+  if (process.argv[2] === '--out') {
+    out = process.argv[3];
+    path = process.argv[4];
+  } else {
+    path = process.argv[2];
+  }
+  
+  if (path.slice(0, 4) === 'http') {
+    webCat(path, out);
+  } else {
+    cat(path, out);
+  }
+  
